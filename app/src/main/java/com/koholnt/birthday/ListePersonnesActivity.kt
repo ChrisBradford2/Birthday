4b0f5b10a56975b35a4b52e78f57015c48a7fcff
Chris <nico.bar2012@gmail.com>
2021-04-03 22:10:21 +0200

    init : ListePersonnesAcivity

---
+++ app/src/main/java/com/koholnt/birthday/ListePersonnesActivity.kt
@@ -0,0 +1,11 @@
+package com.koholnt.birthday
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListePersonnesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_liste_personnes)
+    }
+}
